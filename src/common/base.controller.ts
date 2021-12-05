bb4e4b8f6616940917dc7b686efed988e4b9687f
Edward Arechwa <e.arechwa@digitalcolliers.com>
2021-12-05 21:45:15 +0100

    create abstract controller

---
+++ src/common/base.controller.ts
@@ -0,0 +1,23 @@
+import { IControllerRoute } from "./route.interface";
+import { Router } from "express";
+import { LoggerService } from "../logger/loger.service";
+
+export abstract class BaseController {
+  private readonly _router: Router;
+
+  constructor(private logger: LoggerService) {
+    this._router = Router();
+  }
+
+  get router() {
+    return this._router;
+  }
+
+  protected bindRoutes(routes: IControllerRoute[]) {
+    for (const route of routes) {
+      this.logger.log(`[${route.method}] ${route.path}`);
+      const handler = route.func.bind(this);
+      this.router[route.method](route.path, handler);
+    }
+  }
+}
