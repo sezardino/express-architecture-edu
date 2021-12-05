d8eea53b281b85a87a6292708215a15ad5abfcee
Edward Arechwa <e.arechwa@digitalcolliers.com>
2021-12-05 21:28:12 +0100

    add logger

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express, { Express } from "express";
 import { Server } from "http";
+import { LoggerService } from "./logger/loger.service";
 
 export class App {
   app: Express;
   server: Server;
   port: number;
+  logger: LoggerService;
 
-  constructor() {
+  constructor(logger: LoggerService) {
     this.app = express();
     this.port = 8000;
+    this.logger = logger;
   }
 
   private useRoutes() {}
