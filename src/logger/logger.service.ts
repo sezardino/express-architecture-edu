db616c3e9b14dd0b710a37b2b9477a7bbb61db78
Edward Arechwa <e.arechwa@gmail.com>
2021-12-06 20:19:44 +0100

    add ilogger

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,6 +1,7 @@
+import { ILogger } from './logger.interface';
 import { Logger } from "tslog";
 
-export class LoggerService {
+export class LoggerService implements ILogger {
   public logger: Logger;
 
   constructor() {
